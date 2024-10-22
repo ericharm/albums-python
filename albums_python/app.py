2ae9d04488960d63bf124756556966700c05a53f
Eric Harm <ericharm@gmail.com>
2024-10-22 02:25:13 -0400

    CORS

--- albums_python/app.py
+++ albums_python/app.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 def create_app() -> Flask:
     app = Flask(__name__)
 
+    CORS(app)
+
     from albums_python.service.album_genres_controller import album_genres_blueprint
     from albums_python.service.albums_controller import albums_blueprint
     from albums_python.service.genres_controller import genres_blueprint
@@ -14,13 +16,6 @@ def create_app() -> Flask:
     app.register_blueprint(album_genres_blueprint)
     app.register_blueprint(genres_blueprint)
     app.register_blueprint(users_blueprint)
-
-    CORS(
-        app,
-        origins="*",
-        allow_headers="*",
-        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    )
 
     return app
 
